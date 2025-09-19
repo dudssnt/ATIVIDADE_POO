bed6f3d9727abd16f10bb16809fa556370ca46ba
hadesds <hadessds@gmail.com>
2025-09-19 11:28:43 -0300

    Removendo JDK do versionamento e adicionando .gitignore

--- SimuladorImprt.java
+++ SimuladorImprt.java
@@ -3,6 +3,7 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class SimuladorImprt {
+    
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner scanner = new Scanner(System.in);
